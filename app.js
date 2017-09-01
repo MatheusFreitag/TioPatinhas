1fc02caa648caade381b901b84f679459d28299f
Matheus Freitag <matheus.freitag@prodigious.com>
2017-09-01 12:46:17 -0300

    Bot conversando. Precisa adicionar novas moedas

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const http           = require('http');
@@ -13,159 +15,129 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+var callAPIAI = function(id,text){
 
-app.speechHandler = function(text, id, cb) {
-    if(text && (typeof text === 'string' )){
-        text = helpers.rsc(text);
-    }
-    else{
-        text = "Error Emoji"
-    }
-    
+    text = helpers.rsc(text);
 
     var reqObj = {
-      url: 'https://api.api.ai/v1/query?v=20150910',
-      headers: {
-        "Content-Type":"application/json",
-        "Authorization":"Bearer d690f1cc9c714f218ea43d1d60d4e15c"
-      },
-      method: 'POST',
-      json: {
-        "query":text,
-        "lang":"pt-br",
-        "sessionId":id
-      }
-    };
-
-    //console.log("\n\n\n\n\n" + reqObj.json.query + "\n\n\n\n\n");
-
-    var chamarFixer = function(speech){
-        request("http://api.fixer.io/latest?base=BRL",function(error, response, body){
-            if (error) {
-                console.log('Error sending message: ', JSON.stringify(error));
-                cb(false)
-              } 
-            else {
-                var cotacoes = JSON.parse(body);
-                convertionHandler(speech,cotacoes, montarCallback);  
-            }       
-        });
-    }
-
-    var chamarAPIAI = function(){
-        request(reqObj, function(error, response, body){
-            if (error) {
-                console.log('Error sending message: ', JSON.stringify(error));
-                cb(false)
-              } 
-              else {
-                var speech = body.result.fulfillment.speech;
-                console.log("Mensagem chegou: " + speech);
-                chamarFixer(body);
-              }       
-        });
-    }
-
-    var convertionHandler = function (body, cotacoes) { 
-
-        if(String(body.result.action) !== "Converter"){
-            return montarCallback(body.result.fulfillment.speech);
+        url: 'https://api.api.ai/v1/query?v=20150910',
+        headers: {
+          "Content-Type":"application/json",
+          "Authorization":"Bearer d690f1cc9c714f218ea43d1d60d4e15c"
+        },
+        method: 'POST',
+        json: {
+          "query":text,
+          "lang":"pt-br",
+          "sessionId":id
         }
-
-        var speech = body.result.fulfillment.speech;
-
-        var moedaOrigem = body.result.parameters.moeda;
-        var moedaDestino = body.result.parameters.moeda1;
-        var valor = body.result.parameters.number;
-        var resultado;
-        var multiplicador;
-
-        switch(moedaOrigem){
-            case "reais":
-                    multiplicador = cotacoes.rates.USD;
-                    resultado = Number(valor) * Number(multiplicador);
-                    break;
-
-            case "dolares":
-                    multiplicador = cotacoes.rates.USD;
-                    resultado = Number(valor) / Number(multiplicador);
-                    break;
-        }
-
-        speech = speech.replace("YY", String(resultado));
-        
-        return montarCallback(speech);
-    }
-        
-
-    var montarCallback = function(speech){
-        cb(speech);
     }
 
-    chamarAPIAI();
+    request(reqObj, function(error, response, body){
+        if (error) {
+            console.log('Error sending message: ', JSON.stringify(error));
+            cb(false)
+        } 
+        else {
+            if(body.result.action !== "Converter"){
+                sendText(id,body.result.fulfillment.speech);
+            }
+            else{
+                var resposta = body.result.fulfillment.speech;
+                console.log("Mensagem chegou: " + resposta);
+                callFixerIO(id, resposta, body);
+            }
+            
+        }       
+    });    
+}
 
+var callFixerIO = function(id,resposta, apiai){
+    request("http://api.fixer.io/latest?base=BRL",function(error, response, body){
+        if (error) {
+            console.log('Error sending message: ', JSON.stringify(error));
+            return false;
+        } 
+        else {
+            var fixer = JSON.parse(body);
+            convertionHandler(resposta, id, apiai, fixer.rates);
+        }       
+    });
+}
+
+var convertionHandler = function (text, id, apiai, cotacoes) { 
+    var moedaOrigem = apiai.result.parameters.moeda[0];
+    var moedaDestino = apiai.result.parameters.moeda1[0];
+    var valor = apiai.result.parameters.number;
+    var resultado;
+    var multiplicador;
+
+    switch(moedaOrigem){
+        case "reais":
+                multiplicador = cotacoes.USD;
+                resultado = Number(valor) * Number(multiplicador);
+                break;
+
+        case "dólares":
+                multiplicador = cotacoes.USD;
+                resultado = Number(valor) / Number(multiplicador);
+                break;
+    }
+
+    text = text.replace("YY", String(resultado.toFixed(2)));
+    
+    sendText(id, text);
 }
 
 
-app.messageHandler = function(text, id, cb) {
-    var data = {
-      "recipient":{
-          "id":id
-      },
-      "message":{
-          "text":text
-      }
-    };
 
-    var reqObj = {
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token:token},
-      method: 'POST',
-      json: data
-    };
+var sendText = function(id,text){
+    let message = {text: text}
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs : {access_token: token},
+        method: "POST",
+        json: {
+			recipient: {id: id},
+			message : message,
+        }, 
+        function(error, response,body) {
+            if (error) {
+                console.log("sending error")
+            } 
+            else if (response.body.error) {
+                console.log("response body error")
+            }
+        }
+    })
 
-    request(reqObj, function(error, response, body) {
-      if (error) {
-        console.log('Error sending message: ', JSON.stringify(error));
-        cb(false)
-      } 
-      else if (response.body.error) {
-        console.log("API Error: " + JSON.stringify(response.body.error));
-        cb(false)
-      } 
-      else{
-        cb(true)
-      }
-    });
+    //return true
 }
 
 
 app.post('/fb', function(req, res){
-    console.log("\nUma vez\n");
-    var id = req.body.entry[0].messaging[0].sender.id;
-    var text = req.body.entry[0].messaging[0].message.text;
 
-    app.speechHandler(text, id, function(speech){
-      app.messageHandler(speech, id, function(result){
-      });
-    });
-
-    res.send(req.body);
+    if(req.body.entry){
+        var id = req.body.entry[0].messaging[0].sender.id;
+        var text = req.body.entry[0].messaging[0].message.text;
+    
+        callAPIAI(id, text);
+        res.sendStatus(200);
+    }
+    
+    
 });
 
 
 app.get('/fb', function(req, res) {
   if (req.query['hub.verify_token'] === "botTioPatinhas") {
      res.send(req.query['hub.challenge']);
-   } else {
+   } 
+   else {
      res.send('Error, wrong validation token');
    }
 });
 
-// create a health check endpoint
-app.get('/health', function(req, res) {
-  res.send('okay');
-});
 
 // set port
 app.set('port', process.env.PORT || 8080);
